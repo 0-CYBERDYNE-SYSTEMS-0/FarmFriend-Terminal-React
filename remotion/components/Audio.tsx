9b8053850656c02c358af42512ec03788bed13b1
0-CYBERDYNE-SYSTEMS-0 <craigs.seller.sixx@gmail.com>
2026-01-23 20:03:40 -0600

    Cleanup repo artifacts and remove slop docs

--- remotion/components/Audio.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useCurrentFrame } from 'remotion';
-
-// Audio component - placeholder for background music
-// When you have audio file, uncomment and add path:
-// <Audio src="path/to/audio.mp3" />
-export const Audio: React.FC = () => {
-  return null;
-};
